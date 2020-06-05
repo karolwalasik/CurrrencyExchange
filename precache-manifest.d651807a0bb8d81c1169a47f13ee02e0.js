c5c65c8f783d764704fd35a55a8e791e2d90acbe
Karol Walasik <k.walasik@getprintbox.com>
2020-06-05 02:02:24 +0200

    Updates

---
+++ precache-manifest.d651807a0bb8d81c1169a47f13ee02e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0316cbc123843df122364e4171c7f0a",
+    "url": "/karolwalasik.github.io/CurrencyExchange/index.html"
+  },
+  {
+    "revision": "d3a085b142389130c251",
+    "url": "/karolwalasik.github.io/CurrencyExchange/static/js/2.c0afc23e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/karolwalasik.github.io/CurrencyExchange/static/js/2.c0afc23e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261daabb9198ae96d621",
+    "url": "/karolwalasik.github.io/CurrencyExchange/static/js/main.362aec60.chunk.js"
+  },
+  {
+    "revision": "3c82609d535f45016bd5",
+    "url": "/karolwalasik.github.io/CurrencyExchange/static/js/runtime-main.4cba5750.js"
+  }
+]);
